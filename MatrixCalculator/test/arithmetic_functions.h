290671c1056da475b4d7fc8304323eaee9294386
Margot <64271198+Margot-Jones@users.noreply.github.com>
2020-05-05 14:18:16 +0300

    Update arithmetic_functions.h

--- MatrixCalculator/test/arithmetic_functions.h
+++ MatrixCalculator/test/arithmetic_functions.h
@@ -1,10 +1,10 @@
 #ifndef ARITHMETIC_FUNCTIONS_H
 #define ARITHMETIC_FUNCTIONS_H
 
-void function_Addition(int** A_matrix, int** B_matrix, int** C_matrix, int size);
-void function_Difference(int** A_matrix, int** B_matrix, int** C_matrix, int size);
-void function_TransposeA(int** A_matrix, int** B_matrix, int** C_matrix, int size);
-void function_TransposeB(int** A_matrix, int** B_matrix, int** C_matrix, int size);
-void function_Multiplication(int** A_matrix, int** B_matrix, int** C_matrix, int size);
+void Add(int** A_matrix, int** B_matrix, int** C_matrix, int size);
+void Dif(int** A_matrix, int** B_matrix, int** C_matrix, int size);
+void TransA(int** A_matrix, int** B_matrix, int** C_matrix, int size);
+void TransB(int** A_matrix, int** B_matrix, int** C_matrix, int size);
+void Mult(int** A_matrix, int** B_matrix, int** C_matrix, int size);
 
 #endif // ARITHMETIC_FUNCTIONS_H
